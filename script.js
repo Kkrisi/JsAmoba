29cec7222619c485e5dd8c2187a8330e2acc4fd4
Kádár Kristóf Péter (SZF_2023_2025) <kadar.kristof.peter1@diak.szamalk-szalezi.hu>
2024-03-14 09:31:34 +0100

    oraifolytatas

--- script.js
+++ script.js
@@ -1,17 +1,35 @@
 window.addEventListener("load", function () {
-    elsoFunkcio();
+    NegyzetGeneralas();
+    Kattintas();
    
 });
 
-function elsoFunkcio() {
+
+let jatekos = 0;
+
+function NegyzetGeneralas() {
     const DivELEM = document.getElementById("palya")
     
     let txt = "";
     for(let i = 0; i < 9; i++){
-        txt += `<div class="negyzet"><p class="alak"></p></div>`
+        txt += `<div class="negyzet"></div>`
     }
     DivELEM.innerHTML += txt
 }
+
+
+function Kattintas(){
+    const Negyzet = document.getElementsByClassName("negyzet")
+
+    for (let i = 0; i < Negyzet.length; i++) {
+        Negyzet[i].addEventListener("click", function (event){
+            event.target.style.color = "#505d5b";
+            event.target.innerHTML = "X";
+            jatekos = 1;
+        });
+      }
+}
+
 
 
 
